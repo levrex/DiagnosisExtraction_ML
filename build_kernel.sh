d04aad5a6ffdfffab7ec49751edb1ce6c1a7e3c7
tdmaarseveen <T.D.Maarseveen@lumc.nl>
2019-07-25 13:19:13 +0200

    Adding script for initializing the required kernel

---
+++ build_kernel.sh
@@ -0,0 +1,17 @@
+# Dependencies: conda
+
+# Create a machine learning environment
+
+conda create -n ml_env python=3.5
+
+conda activate ml_env
+
+# If pip cant find the module try command below: 
+#    pip install --upgrade pip 
+
+pip install -r requirements.txt
+
+# link custom environment with modules to a kernel in jupyter
+ipython kernel install --user --name=diagnosis_ra
+
+conda deactivate ml_env
