9925c58480bfde06425e2b78240771b9de5d0a83
tdmaarseveen <T.D.Maarseveen@lumc.nl>
2019-12-27 17:55:48 +0100

    pytest example for github

---
+++ .github/tests/simpleTest.py
@@ -0,0 +1,17 @@
+
+def test_empty():
+    """
+    PyTest searches for modules that start with "test" to run
+    (by default)
+    """
+    pass
+
+
+
+
+
+def donot_test():
+    """
+    Name doesn't start with 'test' -> so won't run
+    """
+    pass
